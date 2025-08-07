98e55d112fb0a4749a31b02b1d930e956f713c49
nestera11 <bjik0chik@gmail.com>
2025-08-07 15:55:09 +0400

    +

--- nipak_gowebsockets/README.md
+++ nipak_gowebsockets/README.md
@@ -1,3 +1,5 @@
 package wsserver: создаем структуру "wsSrv" для хранения состояния сервиса. 
 
-go get github.com/gorilla/websocket
+go get 
+github.com/gorilla/websocket
+github.com/sirupsen/logrus
--- nipak_gowebsockets/internal/wsserver/server.go
+++ nipak_gowebsockets/internal/wsserver/server.go
@@ -1,14 +1,24 @@
 package wsserver
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gorilla/websocket"
+	log "github.com/sirupsen/logrus"
+)
+
+const (
+	templDir = "./web/templates/html"
+)
 
 type WSServer interface {
 	Start() error
 }
 
 type wsServer struct {
-	srv *http.Server
-	mux *http.ServeMux
+	srv      *http.Server
+	mux      *http.ServeMux
+	upgrader *websocket.Upgrader
 }
 
 func New(addr string) WSServer {
@@ -19,15 +29,31 @@ func New(addr string) WSServer {
 			Addr:    addr,
 			Handler: mux,
 		},
+		upgrader: &websocket.Upgrader{
+			ReadBufferSize:  1024,
+			WriteBufferSize: 1024,
+		},
 	}
 	return &wsServer
 }
 
 func (s *wsServer) Start() error {
+	s.mux.Handle("/", http.FileServer(http.Dir(templDir)))
 	s.mux.HandleFunc("/test", s.testHandler)
+	s.mux.HandleFunc("/ws", s.wsHandler)
 	return s.srv.ListenAndServe()
 }
 
 func (s *wsServer) testHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Test is successful"))
+}
+
+func (s *wsServer) wsHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := s.upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Errorf("Error with ws connection: %v", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	log.Infof(conn.RemoteAddr().String())
 }
