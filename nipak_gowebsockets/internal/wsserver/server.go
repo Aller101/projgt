1dfb7af004f52b3d02ea255ce0128524bf39c477
nestera11 <bjik0chik@gmail.com>
2025-08-07 16:52:56 +0400

    +

--- nipak_gowebsockets/internal/wsserver/server.go
+++ nipak_gowebsockets/internal/wsserver/server.go
@@ -2,6 +2,8 @@ package wsserver
 
 import (
 	"net/http"
+	"sync"
+	"time"
 
 	"github.com/gorilla/websocket"
 	log "github.com/sirupsen/logrus"
@@ -16,9 +18,12 @@ type WSServer interface {
 }
 
 type wsServer struct {
-	srv      *http.Server
-	mux      *http.ServeMux
-	upgrader *websocket.Upgrader
+	srv       *http.Server
+	mux       *http.ServeMux
+	upgrader  *websocket.Upgrader
+	wsClients map[*websocket.Conn]struct{}
+	mutex     *sync.RWMutex
+	broadcast chan *wsMessage
 }
 
 func New(addr string) WSServer {
@@ -33,6 +38,9 @@ func New(addr string) WSServer {
 			ReadBufferSize:  1024,
 			WriteBufferSize: 1024,
 		},
+		wsClients: map[*websocket.Conn]struct{}{},
+		mutex:     &sync.RWMutex{},
+		broadcast: make(chan *wsMessage),
 	}
 	return &wsServer
 }
@@ -41,6 +49,7 @@ func (s *wsServer) Start() error {
 	s.mux.Handle("/", http.FileServer(http.Dir(templDir)))
 	s.mux.HandleFunc("/test", s.testHandler)
 	s.mux.HandleFunc("/ws", s.wsHandler)
+	go s.writeToClients()
 	return s.srv.ListenAndServe()
 }
 
@@ -55,5 +64,47 @@ func (s *wsServer) wsHandler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	log.Infof(conn.RemoteAddr().String())
+	log.Infof("Client with address %s\n", conn.RemoteAddr().String())
+	s.mutex.Lock()
+	s.wsClients[conn] = struct{}{}
+	s.mutex.Unlock()
+
+	go s.readFromClient(conn)
+}
+
+func (s *wsServer) readFromClient(conn *websocket.Conn) {
+	for {
+		msg := new(wsMessage)
+		err := conn.ReadJSON(msg)
+		if err != nil {
+			log.Errorf("Error with reading from ws: %v", err)
+			break
+		}
+		msg.IPAddress = conn.RemoteAddr().String()
+		msg.Time = time.Now().Format("15:00")
+		s.broadcast <- msg
+	}
+	s.mutex.Lock()
+	delete(s.wsClients, conn)
+	s.mutex.Unlock()
+}
+
+func (s *wsServer) writeToClients() {
+	for msg := range s.broadcast {
+		s.mutex.RLock()
+		for client := range s.wsClients {
+
+			// if err := client.WriteJSON(msg); err != nil {
+			// 	log.Errorf("Error with writing msg: %v", err)
+			// 	continue
+			// }
+
+			func() {
+				if err := client.WriteJSON(msg); err != nil {
+					log.Errorf("Error with writing msg: %v", err)
+				}
+			}()
+		}
+		s.mutex.RUnlock()
+	}
 }
